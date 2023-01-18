1a568c2bea726b3b9e154bacd0510fac976df0c8
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-01-18 15:45:23 +0100

    delete css

--- app.vue
+++ app.vue
@@ -5,10 +5,8 @@
       <div class="main-h-full">
 
 
-
-
         <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8">
-          <div class="border-2 border-red-500 b md:block xs-col-span-1 xl:col-span-2">
+          <div class="md:block xs-col-span-1 xl:col-span-2">
             <div class="sticky top-0">
               <SidebarLeft />
             </div>
