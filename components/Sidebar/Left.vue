e9a7aa8fbd419440337efa95a91be88fe860d457
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-01-18 15:48:31 +0100

    adding link

--- components/Sidebar/Left.vue
+++ components/Sidebar/Left.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
-    <div class="w-8 h-8">
-        <LogoTwitter />
+    <div>
+        <div class="p-2 my-2 hover:bg-red-300 hover:rounded-full w-min">
+            <nuxt-link to="/">
+                <div class="w-8 h-8">
+                    <LogoTwitter />
+                </div>
+            </nuxt-link>
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup>
