0943c8ea61354f3c8fbee8c978693bfa51612d08
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-01-18 15:59:41 +0100

    adding composable

--- components/Sidebar/Left.vue
+++ components/Sidebar/Left.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        <div class="p-2 my-2 hover:bg-red-300 hover:rounded-full w-min">
+    <div class="flex flex-col h-screen">
+        <div class="p-2 my-2 hover:bg-blue-50 rounded-full w-min
+        dark:hover:bg-white/20" :class="transition"
+        >
             <nuxt-link to="/">
                 <div class="w-8 h-8">
                     <LogoTwitter />
@@ -11,5 +13,5 @@
 </template>
 
 <script setup>
-
+    const transition = 'transition ease-in-out duration-350'
 </script>
