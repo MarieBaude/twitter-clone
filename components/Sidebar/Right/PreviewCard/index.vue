ab62592ccce21a05a19433330acd28bd23673296
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-01-19 11:47:45 +0100

    adding show more in right

--- components/Sidebar/Right/PreviewCard/index.vue
+++ components/Sidebar/Right/PreviewCard/index.vue
@@ -3,6 +3,14 @@
         <h1 class="p-3 text-xl font-extrabold text-gray-900 border-b dark:text-white" :class="twitterBorderColor">
             {{ props.title }}
         </h1>
+
+        <slot></slot>
+
+        <div class="p-3 text-sm text-blue-400 cursor-pointer hover:bg-gray-100 dark:hover:bg-dim-300"
+            :class="defaultTransition">
+            Show more
+        </div>
+
     </div>
 </template>
 
