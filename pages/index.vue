ca0453a2ad42423454fbe4f522cefff34e54529c
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-01-19 15:49:33 +0100

    adding template main page

--- pages/index.vue
+++ pages/index.vue
@@ -2,20 +2,12 @@
     <div>
         <MainSection title="Home" :loading="loading">
 
-            <Head>
-                <Title>Home / Twitter</Title>
-            </Head>
-
-            <div class="border-b" :class="twitterBorderColor">
-                <TweetForm :user="user" @on-success="handleFormSuccess" />
-            </div>
-
-            <TweetListFeed :tweets="homeTweets" />
+            main content
 
         </MainSection>
     </div>
 </template>
 
-<script>
-
+<script setup>
+const loading = ref(false);
 </script>
