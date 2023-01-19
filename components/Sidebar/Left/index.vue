8ac7fadcca81b02723e1becdc76eac5007005f1a
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-01-19 11:11:01 +0100

    adding all link in left menu

--- components/Sidebar/Left/index.vue
+++ components/Sidebar/Left/index.vue
@@ -1,28 +1,87 @@
 <template>
     <div class="flex flex-col h-screen">
         <div class="p-2 my-2 hover:bg-blue-50 rounded-full w-min
-        dark:hover:bg-white/20" :class="defaultTransition"
-        >
+        dark:hover:bg-white/20" :class="defaultTransition">
             <nuxt-link to="/">
                 <div class="w-8 h-8">
                     <LogoTwitter />
                 </div>
             </nuxt-link>
         </div>
-        
-        <SidebarLeftTab active>
-            <template v-slot:icon>
-                <HomeIcon />
-            </template>
-            <template v-slot:name>
-                Home
-            </template>
-        </SidebarLeftTab>
+
+        <div class="mt-2 space-y-8">
+            
+            <SidebarLeftTab active>
+                <template v-slot:icon>
+                    <HomeIcon />
+                </template>
+                <template v-slot:name>
+                    Home
+                </template>
+            </SidebarLeftTab>
+            
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <HashtagIcon />
+                </template>
+                <template v-slot:name>
+                    Explore
+                </template>
+            </SidebarLeftTab>
+
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <InboxIcon />
+                </template>
+                <template v-slot:name>
+                    Messages
+                </template>
+            </SidebarLeftTab>
+
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <BookmarkIcon />
+                </template>
+                <template v-slot:name>
+                    Bookmarks
+                </template>
+            </SidebarLeftTab>
+
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <DocumentTextIcon />
+                </template>
+                <template v-slot:name>
+                    Lists
+                </template>
+            </SidebarLeftTab>
+
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <UserIcon />
+                </template>
+                <template v-slot:name>
+                    Profile
+                </template>
+            </SidebarLeftTab>
+
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <DotsCircleHorizontalIcon />
+                </template>
+                <template v-slot:name>
+                    More
+                </template>
+            </SidebarLeftTab>
+
+        </div>
+
+
     </div>
 </template>
 
 <script setup>
-    import { HomeIcon } from "@heroicons/vue/solid"
+import { BellIcon, BookmarkIcon, BookOpenIcon, HashtagIcon, HomeIcon, InboxIcon, DocumentTextIcon, UserIcon, DotsCircleHorizontalIcon } from "@heroicons/vue/solid"
 
-    const { defaultTransition } = useTailwindConfig();
+const { defaultTransition } = useTailwindConfig();
 </script>
