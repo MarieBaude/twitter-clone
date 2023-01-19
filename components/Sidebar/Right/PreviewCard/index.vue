399b776a591b02d3b2381488e2a63fdedbd644c1
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-01-19 11:44:45 +0100

    adding class and js

--- components/Sidebar/Right/PreviewCard/index.vue
+++ components/Sidebar/Right/PreviewCard/index.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="m-2 overflow-hidden border rounded-2xl bg-gray-50 dark:bg-dim-700">
-        <h1>
+    <div class="m-2 overflow-hidden border rounded-2xl bg-gray-50 dark:bg-dim-700" :class="twitterBorderColor">
+        <h1 class="p-3 text-xl font-extrabold text-gray-900 border-b dark:text-white" :class="twitterBorderColor">
             {{ props.title }}
         </h1>
     </div>
 </template>
 
 <script setup>
+const { twitterBorderColor, defaultTransition } = useTailwindConfig();
+
 const props = defineProps({
     title: {
         type: String,
