1c2875951d3460fc538fde8c929b73f60b0d93ec
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-01-19 11:40:38 +0100

    adding class and js

--- components/Sidebar/Right/PreviewCard/index.vue
+++ components/Sidebar/Right/PreviewCard/index.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
-    <h1>
-        {{ props.title }}
-    </h1>
-  </div>
+    <div class="m-2 overflow-hidden border rounded-2xl bg-gray-50 dark:bg-dim-700">
+        <h1>
+            {{ props.title }}
+        </h1>
+    </div>
 </template>
 
-<script>
-
+<script setup>
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    }
+});
 </script>
