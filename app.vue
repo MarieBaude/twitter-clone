78099ce5260e5a959d28d3e1a431893d11cc70ac
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-01-19 11:25:52 +0100

    adding right content

--- app.vue
+++ app.vue
@@ -18,6 +18,13 @@
             <router-view />
           </main>
 
+          <!-- Right Sidebar -->
+          <div class="hidden col-span-12 md:block xl:col-span-4 md:col-span-3">
+            <div class="sticky top-0">
+              <SidebarRight />
+            </div>
+          </div>
+
         </div>
 
       </div>
