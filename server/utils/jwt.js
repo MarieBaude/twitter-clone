d0b4a8335117207dab8bbcea24553fbc94dbeb32
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-01-20 11:02:54 +0100

    adding generateAccessToken content

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,7 +1,11 @@
 import jwt from "jsonwebtoken"
 
 const generateAccessToken = (user) => {
+    const config = useRuntimeConfig()
 
+    return jwt.sign({ userId: user.id }, config.jwtAccessSecret, {
+        expiresIn: '10m'
+    })
 }
 
 const generateRefreshToken = (user) => {
