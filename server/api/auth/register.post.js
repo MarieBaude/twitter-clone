dffb9ef9ef03fa96a7c2b471939951b4fd0c5a21
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-01-20 10:33:04 +0100

    finish register post

--- server/api/auth/register.post.js
+++ server/api/auth/register.post.js
@@ -1,4 +1,6 @@
 import { sendError } from "h3"
+import { createUser } from "../../db/users.js"
+import { userTransformer } from "~~/server/transformers/user.js"
 
 export default defineEventHandler(async (event) => {
     const body = await useBody(event)
@@ -12,6 +14,16 @@ export default defineEventHandler(async (event) => {
     if (password !== repeatPassword) {
         return sendError(event, createError({ statusCode: 400, statusMessage: 'Passwords do not match' }))
     }
+
+    const userData = {
+        username,
+        email,
+        password,
+        name,
+        profileImage: 'https://picsum.photos/200/200'
+    }
+
+    const user = await createUser(userData)
 
     return {
         body: body
