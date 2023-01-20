af7aa9bb7e34b60151e08f34cb774eb34353ec93
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-01-20 11:00:21 +0100

    adding config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from "nuxt/config"
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss']
+    modules: ['@nuxtjs/tailwindcss'],
+
+    runtimeConfig: {
+        jwtAccessSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
+        jwtRefreshSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
+    }
 })
